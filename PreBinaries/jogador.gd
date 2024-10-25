c2bfa270a2bb706396d07ca3ce39bf659a288cc7
José A. Guirado González <trizador@gmail.com>
2024-10-25 16:34:37 +0200

    se sube proyecto con modelos nuevos
    se utiliza un modelo de barbaro para los personajes de prueba

--- PreBinaries/jogador.gd
+++ PreBinaries/jogador.gd
@@ -9,22 +9,27 @@ const GRAVITY = -9.0
 
 
 func _physics_process(delta: float) -> void:
+	if !Input.is_action_pressed("w"):
+		$MeshInstance3D/Barbarian/AnimationPlayer.play("Idle")
+
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta 
-
-
 	else:
 		# Si estamos en el suelo, reseteamos la velocidad en el eje Y
 		velocity.y = 0
 
+
 	if Input.is_action_pressed("w") and Input.is_action_pressed("s"):
 		velocity.x = 0
 		velocity.z = 0
+		$MeshInstance3D/Barbarian/AnimationPlayer.play("Idle")
 
 	elif Input.is_action_pressed("w"):
 		var forwardVector = -Vector3.FORWARD.rotated(Vector3.UP, rotation.y)
 		velocity = -forwardVector * FORWARD_SPEED
+		$MeshInstance3D/Barbarian/AnimationPlayer.play("Running_A")
 		if not is_on_floor():
 			velocity.y -= 5
 
@@ -51,6 +56,9 @@ func _physics_process(delta: float) -> void:
 		rotation.z -= Vector3.ZERO.y + TURN_SPEED #* V_LOOK_SENS
 		rotation.z = clamp(rotation.x, -50, 90)
 		rotation.y -= Vector3.ZERO.y + TURN_SPEED #* M_LOOK_SENS
+	
+
+
 
 
 	move_and_slide()
